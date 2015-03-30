5c505aa6fc3f5e41b0bdedbc9fd0a9202efed567
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-30 21:01:55 +0200

    create map-tree

--- src/demo/transform.clj
+++ src/demo/transform.clj
@@ -3,3 +3,9 @@
 ; We can generate structure trees
 ; We can evaluate abstract syntax trees
 ; how do we go from on to the other?
+
+(defn map-tree [f tree]
+  "Let f operate on all the nodes of the tree"
+  (if (nil? tree)
+    (f tree nil nil)
+    (f tree (map-tree f (:left tree)) (map-tree f (:right tree)))))
