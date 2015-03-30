7543c1a48513a1f7be631f40a6a2ceea219f9a92
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-30 20:40:19 +0200

    provide evaluate function

--- src/demo/evaluate.clj
+++ src/demo/evaluate.clj
@@ -4,3 +4,10 @@
 ; {:operator + :left {:value 1} :right {:value 2}}
 ; we want it to evaluate to 3
 
+(defn evaluate [tree]
+  "evaluate tree to its value"
+  (if (:operator tree)
+    ((:operator tree) (evaluate (:left tree)) (evaluate (:right tree)))
+    (:value tree)))
+
+
