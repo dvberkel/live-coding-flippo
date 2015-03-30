d4a753e94053f825193a26a6d4de83b494d2ba28
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-30 20:05:01 +0200

    provide examples of trees

--- src/demo/tree.clj
+++ src/demo/tree.clj
@@ -3,3 +3,9 @@
 ; Lets generate all tree structures.
 ; 1. nil is a tree; leaf
 ; 2. if L and R are trees, so is { :left L :right R }
+
+; some examples of trees
+nil
+{:left nil :right nil}
+{:left {:left nil :righ :nil} :right nil}
+
