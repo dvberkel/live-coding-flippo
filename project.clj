3f35060ae077ea21b3aa610feb10249085022af3
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-30 16:55:10 +0200

    initialize leiningen project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject demo "0.1.0-SNAPSHOT"
+  :description "Live coding flippo"
+  :url "https://github.com/dvberkel/live-coding-flippo"
+  :license {:name "The MIT License (MIT)"
+            :url "https://github.com/dvberkel/live-coding-flippo/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot demo.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/demo/core.clj
@@ -0,0 +1,7 @@
+(ns demo.core
+  (:gen-class))
+
+(defn -main
+  "Print out a solution to a 24 problem instance"
+  [& args]
+  (println "Hello, World!"))
