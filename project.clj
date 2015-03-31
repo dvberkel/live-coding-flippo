9d1f72625bd7ad300aa3c7f3c2caf81bd64ae507
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-31 09:49:23 +0200

    depend on math.combinatorics

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/dvberkel/live-coding-flippo"
   :license {:name "The MIT License (MIT)"
             :url "https://github.com/dvberkel/live-coding-flippo/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.combinatorics "0.0.8"]]
   :main ^:skip-aot demo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
